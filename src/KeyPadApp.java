9798c05d6293cdf409dcca379ac2420b12a44e0d
Jack Vanlyssel <jvanlyssel@unm.edu>
2023-08-31 13:50:30 -0600

    Updated the keypad with features from the RDD document

--- src/KeyPadApp.java
+++ src/KeyPadApp.java
@@ -7,7 +7,6 @@ import javafx.stage.Stage;
 
 public class KeyPadApp extends Application {
 
-
     @Override
     public void start(Stage primaryStage) {
         GridPane gridPane = new GridPane();
@@ -18,24 +17,37 @@ public class KeyPadApp extends Application {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 int number = i * 3 + j + 1;
-                Button btn = createButton(String.valueOf(number));
+                Button btn = createButton(String.valueOf(number), String.valueOf(number));
                 gridPane.add(btn, j, i);
             }
         }
 
-        Button zeroButton = createButton("0");
-        gridPane.add(zeroButton, 1, 3);
+        Button volumeUpButton = createButton("^", "volume up");
+        Button zeroButton = createButton("0", "0");
+        Button volumeDownButton = createButton("↓", "volume down");
 
-        Scene scene = new Scene(gridPane, 300, 300);
+        gridPane.add(volumeUpButton, 0, 3);
+        gridPane.add(zeroButton, 1, 3);
+        gridPane.add(volumeDownButton, 2, 3);
+
+        Button cancelButton = createButton("X", "cancel");
+        Button asteriskButton = createButton("*", "*");
+        Button enterButton = createButton("O", "enter");
+
+        gridPane.add(cancelButton, 0, 4);
+        gridPane.add(asteriskButton, 1, 4);
+        gridPane.add(enterButton, 2, 4);
+
+        Scene scene = new Scene(gridPane, 300, 400);
         primaryStage.setTitle("KeyPad App");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-    private Button createButton(String text) {
+    private Button createButton(String text, String printText) {
         Button btn = new Button(text);
         btn.setPrefSize(60, 60);
-        btn.setOnAction(event -> System.out.println(text));
+        btn.setOnAction(event -> System.out.println(printText));
         return btn;
     }
 

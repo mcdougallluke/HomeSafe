f07c68648510bd58e7381240d86f913d03080fd0
spoorthimenta <smenta@unm.edu>
2023-09-30 16:57:24 -0600

    pin manager updates

--- src/PINManager.java
+++ src/PINManager.java
@@ -2,12 +2,16 @@
 
 public class PINManager {
     private static final String DEFAULT_PIN = "000000";
+    // The current PIN in use.
     private static String currentPIN = DEFAULT_PIN;
 
+    // Checks if the provided PIN matches the current PIN.
     public static boolean checkPIN(String pin) {
         return currentPIN.equals(pin);
     }
 
+
+    // Sets the current PIN to the provided value if it is of valid length.
     public static boolean setPIN(String pin) {
         if(pin.length() == 6) {
             currentPIN = pin;
@@ -16,6 +20,7 @@ public class PINManager {
         return false;
     }
 
+    // Checks if the current PIN is set to the default value.
     public boolean isDefaultPIN() {
         return currentPIN.equals(DEFAULT_PIN);
     }
